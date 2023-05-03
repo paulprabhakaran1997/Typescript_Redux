8c4da654cb4a09af65010d0221948b4e46b394e1
Paul Prabhakaran <paul@lintcloud.com>
2023-05-03 18:46:35 +0530

    Project Uploaded

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,70 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home/Home';
+import Products from './components/Products/Products';
+import Appliances from './components/Appliances/Appliances';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NotFound from './components/NotFound/404';
+import Login from './components/Login/Login';
+import PublicRouting from './common/Routing/PublicRouting';
+import PrivateRouting from './common/Routing/PrivateRouting';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Layout from './components/Layout';
 
 function App() {
+
+  const data = [
+    { id: 1, name: "Antony", age: 24 },
+    { id: 2, name: "Vinish", age: 26 },
+    { id: 3, name: "Aswin", age: 25 },
+    { id: 4, name: "Elan", age: 29 },
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path='/login' element={
+            <PublicRouting>
+              <Login />
+            </PublicRouting>
+          } />
+
+          {/* Wrapping All Child Component as Outlet to Layout component  */}
+
+          {/* Start */}
+
+          <Route element={<Layout />}>
+            <Route path='/' element={
+              <PrivateRouting>
+                <Home name='Paul' age={25} friendsList={data} />
+              </PrivateRouting>
+            } />
+            <Route path='/home' element={
+              <PrivateRouting>
+                <Home name='Paul' age={25} friendsList={data} />
+              </PrivateRouting>
+            } />
+            <Route path='products' element={
+              <PrivateRouting>
+                <Products />
+              </PrivateRouting>
+            } />
+            <Route path='appliances' element={
+              <PrivateRouting>
+                <Appliances />
+              </PrivateRouting>
+            } />
+          </Route>
+
+          {/* End */}
+
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </Router>
+
     </div>
   );
 }
